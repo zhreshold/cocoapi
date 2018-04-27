5f371b661fdab6ea824e768dd96942340af3e1ec
Joshua Zhang <cheungchih@gmail.com>
2018-04-27 11:01:41 -0700

    sdist

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,26 +1,53 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-from distutils.extension import Extension
-import numpy as np
+from __future__ import print_function
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
 
-# To install and compile to your anaconda/python site-packages, simply run:
-# $ pip install git+https://github.com/philferriere/cocoapi.git#subdirectory=PythonAPI
-# Note that the original compile flags below are GCC flags unsupported by the Visual C++ 2015 build tools.
-# They can safely be removed.
+# This hack delays importing cythonize until after setup_requires ensures that it exists.
+class cythonize(list):
+    def __init__(self, ext):
+        self.ext = ext
+
+    def __iter__(self):
+        if self.ext is not None:
+            from Cython.Build import cythonize
+            list.__init__(self, cythonize(self.ext))
+            self.ext = None
+        return list.__iter__(self)
+
+# hack to make sure numpy can be imported if numpy is not pre-installed
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+# To compile and install locally run "python setup.py build_ext --inplace"
+# To install library to Python site-packages run "python setup.py build_ext install"
 
 ext_modules = [
     Extension(
         'pycocotools._mask',
-        sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
-        extra_compile_args=[] # originally was ['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        sources=['src/maskApi.c', 'pycocotools/_mask.pyx'],
+        include_dirs = ['src'],
+        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
 ]
 
+long_description = open('README.txt').read()
+
 setup(name='pycocotools',
+      license='FreeBSD',
+      author='http://cocodataset.org/',
+      description='Python APIs for MS COCO dataset.',
+      long_description=long_description,
       packages=['pycocotools'],
       package_dir = {'pycocotools': 'pycocotools'},
-      version='2.0',
-      ext_modules=
-          cythonize(ext_modules)
+      version='2.0.0.post2',
+      url='https://github.com/zhreshold/cocoapi',
+      setup_requires=['cython', 'setuptools>=18.0'],
+      install_requires=['numpy', 'matplotlib'],
+      cmdclass={'build_ext': build_ext},
+      ext_modules=cythonize(ext_modules)
       )
